10a8324749dec60c76ab53e09fabcdf2b3522d18
ViteSpirite <boulangermatheo62@gmail.com>
2023-08-27 10:38:44 +0200

    initialize sqlite

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      storage: 'database.sqlite',
+      synchronize: true,
+      autoLoadModels: true,
+      models: [__dirname + '/**/*.model.ts'],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
